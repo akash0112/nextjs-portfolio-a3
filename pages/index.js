4c5f9df98b7f601ec1b381662e581a9b6be1deb3
akash0112 <akashyadav0112@gmail.com>
2022-10-31 01:28:21 +0530

    made responsive

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,15 @@ import { BsFillMoonStarsFill } from 'react-icons/bs'
 import { AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai'
 import Image from 'next/image'
 import deved from '../public/dev-ed-wave.png'
+import design from '../public/design.png'
+import code from '../public/code.png'
+import consulting from '../public/consulting.png'
+import web1 from '../public/web1.png'
+import web2 from '../public/web2.png'
+import web3 from '../public/web3.png'
+import web4 from '../public/web4.png'
+import web5 from '../public/web5.png'
+import web6 from '../public/web6.png'
 export default function Home() {
   return (
     <div>
@@ -12,31 +21,113 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className=' bg-white px-10'>
-        <section className='h-screen min-h-screen'>
-          <nav className=' py-10 mb-12 flex justify-between'>
-            <h1 className=' text-xl font-burtons'>Portfolio</h1>
-            <ul className='flex items-center'>
-              <li><BsFillMoonStarsFill className=' cursor-pointer text-xl' /></li>
-              <li><a className=' bg-gradient-to-r  from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href='#'>Resume</a></li>
-            </ul>
-          </nav>
-          <div className=' text-center p-10'>
-            <h2 className=' text-5xl py-2 text-teal-600 font-medium'>Akash Yadav</h2>
-            <h3 className=' text-2xl py-2 '>Developer and Designer</h3>
-            <p className=' text-md py-5 leading-5 text-gray-800'>Freelancer Providing services for programming and design content needs.
-              join me down below and let&apos;s get cracking!
-            </p>
-          </div>
-          <div className=' text-5xl flex py-3 text-gray-600 justify-center gap-16'>
-            <AiFillTwitterCircle />
-            <AiFillLinkedin />
-            <AiFillYoutube />
-          </div>
-          <div className=' relative mx-auto bg-gradient-to-b from-teal-500 w-80 h-80 rounded-full mt-20 overflow-hidden'>
-            <Image src={deved} layout="fill" objectFit="cover" />
-          </div>
-        </section>
+      <main className=' bg-white px-10 md:px-20 lg:px-40'>
+        <div className='h-screen min-h-screen'>
+          <section>
+            <nav className=' py-10 mb-12 flex justify-between'>
+              <h1 className=' text-xl font-burtons'>Portfolio</h1>
+              <ul className='flex items-center'>
+                <li><BsFillMoonStarsFill className=' cursor-pointer text-xl' /></li>
+                <li><a className=' bg-gradient-to-r  from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href='#'>Resume</a></li>
+              </ul>
+            </nav>
+            <div className=' text-center p-10'>
+              <h2 className=' text-5xl py-2 text-teal-600 font-medium md:text-6xl'>Akash Yadav</h2>
+              <h3 className=' text-2xl py-2 md: text-3xl'>Developer and Designer</h3>
+              <p className=' text-md py-5 leading-5 text-gray-800 md:text-xl max-w-xl mx-auto'>Freelancer Providing services for programming and design content needs.
+                join me down below and let&apos;s get cracking!
+              </p>
+            </div>
+            <div className=' text-5xl flex py-3 text-gray-600 justify-center gap-16'>
+              <AiFillTwitterCircle />
+              <AiFillLinkedin />
+              <AiFillYoutube />
+            </div>
+            <div className=' relative mx-auto bg-gradient-to-b from-teal-500 w-80 h-80 rounded-full mt-20 overflow-hidden'>
+              <Image src={deved} layout="fill" objectFit="cover" />
+            </div>
+          </section>
+          <section>
+            <div className=' text-center p-10'>
+              <h3 className=' text-2xl py-2 '>Services I Offer you</h3>
+              <p className=' text-md py-5 leading-5 text-gray-800'>
+                Since the beginning of my journey as a freelance react developer. i have done remote work
+                for <span className='text-teal-500'>companies</span> and <span className='text-teal-500'>startups</span>.i offer wide range of services.
+              </p>
+            </div>
+            <div className='lg:flex gap-10' >
+              <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+                <Image className=' mx-auto' src={design} width={100} height={100} />
+                <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
+                <p className='py-2'>Creating elements designs for suited for your needs</p>
+                <h4 className=' text-gray-600 py-4'>
+                  <p className=' text-gray-800 py-2'>Photoshop</p>
+                  <p className=' text-gray-800 py-2'>Illustrator</p>
+                  <p className=' text-gray-800 py-2'>Figma</p>
+                </h4>
+              </div>
+              <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+                <Image className=' mx-auto' src={design} width={100} height={100} />
+                <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
+                <p className='py-2'>Creating elements designs for suited for your needs</p>
+                <h4 className=' text-gray-600 py-4'>
+                  <p className=' text-gray-800 py-2'>Photoshop</p>
+                  <p className=' text-gray-800 py-2'>Illustrator</p>
+                  <p className=' text-gray-800 py-2'>Figma</p>
+                </h4>
+              </div>
+              <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+                <Image className=' mx-auto' src={code} width={100} height={100} />
+                <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
+                <p className='py-2'>Creating elements designs for suited for your needs</p>
+                <h4 className=' text-gray-600 py-4'>
+                  <p className=' text-gray-800 py-2'>Photoshop</p>
+                  <p className=' text-gray-800 py-2'>Illustrator</p>
+                  <p className=' text-gray-800 py-2'>Figma</p>
+                </h4>
+              </div>
+              <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+                <Image className=' mx-auto' src={consulting} width={100} height={100} />
+                <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
+                <p className='py-2'>Creating elements designs for suited for your needs</p>
+                <h4 className=' text-gray-600 py-4'>
+                  <p className=' text-gray-800 py-2'>Photoshop</p>
+                  <p className=' text-gray-800 py-2'>Illustrator</p>
+                  <p className=' text-gray-800 py-2'>Figma</p>
+                </h4>
+              </div>
+            </div>
+          </section>
+          <section>
+            <div>
+              <h3 className=' text-3xl py-1'>portfolio</h3>
+              <p className=' text-md py-5 leading-5 text-gray-800'>
+                Since the beginning of my journey as a freelance react developer. i have done remote work
+                for <span className='text-teal-500'>companies</span> and <span className='text-teal-500'>startups</span>.i offer wide range of services.
+              </p>
+            </div>
+            <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
+            <div className='basis-1/3 flex-1'>
+              <Image src={web1} className=" rounded-lg object-cover" width={"100%"} height={"100%"}/>
+            </div>
+            <div className='basis-1/3 flex-1'>
+              <Image src={web2} className=" rounded-lg object-cover" width={"100%"} height={"100%"}/>
+            </div>
+            <div className='basis-1/3 flex-1'>
+              <Image src={web3} className=" rounded-lg object-cover" width={"100%"} height={"100%"}/>
+            </div>
+            <div className='basis-1/3 flex-1'>
+              <Image src={web4} className=" rounded-lg object-cover" width={"100%"} height={"100%"}/>
+            </div>
+            <div className='basis-1/3 flex-1'>
+              <Image src={web5} className=" rounded-lg object-cover" width={"100%"} height={"100%"}/>
+            </div>
+            <div className='basis-1/3 flex-1'>
+              <Image src={web6} className=" rounded-lg object-cover" width={"100%"} height={"100%"}/>
+            </div>
+            </div>
+          </section>
+        </div>
       </main>
     </div>
   )
