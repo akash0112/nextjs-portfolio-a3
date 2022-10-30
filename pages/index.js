000ad2896f21467d52bda3f79afa8850cb9c5c90
akash0112 <akashyadav0112@gmail.com>
2022-10-31 00:04:46 +0530

    again

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
             <h2 className=' text-5xl py-2 text-teal-600 font-medium'>Akash Yadav</h2>
             <h3 className=' text-2xl py-2 '>Developer and Designer</h3>
             <p className=' text-md py-5 leading-5 text-gray-800'>Freelancer Providing services for programming and design content needs.
-              join me down below and let's get cracking!
+              join me down below and let&apos;s get cracking!
             </p>
           </div>
           <div className=' text-5xl flex py-3 text-gray-600 justify-center gap-16'>
